5d0c2907dc0bea742465e6a99f5bec6150d46a65
ppipee <kittapon.j@ku.th>
2019-10-04 12:26:11 +0700

    [core]: setup package

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+congst graphqlHTTP = require('express-graphql');
+// const schema = require('./schema/schema')
+
+// require('dotenv').config();
+
+const app = express();
+const mongoose = require('mongoose');
+const uri = process.env.IWGU_URI;
+
+// mongoose
+//     .connect(uri, {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useUnifiedTopology: true,
+//     })
+//     .then(() => {
+//         console.log('Connect to MongoDB')
+//     })
+//     .catch(err => console.log(err));
+
+// mongoose.connection.once('open', () => {
+//     console.log('conneted to database');
+// });
+// app.use('/graphql', graphqlHTTP({
+//     schema,
+//     graphiql: true
+// }));
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+}); 
