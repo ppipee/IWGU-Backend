4d3d88ac4ea48d8d706a6b122744f836afb681fd
ppipee <kittapon.j@ku.th>
2019-10-09 15:41:40 +0700

    fix port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const schema = require('./schema')
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const uri = process.env.IWGU_URI;
 mongoose
